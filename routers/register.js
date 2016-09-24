f37bdeaf1d19a56554c500bf5e6ad26e13177faf
Aida Benito <anbenito@yahoo.com.ar>
2016-09-24 20:02:09 -0300

    llamada backend ok

--- routers/register.js
+++ routers/register.js
@@ -9,21 +9,20 @@ var registerPharmacy = function(req, res) {
     var form=req.body;
     console.log(form.fantasyName);
 
-    confirmEntity(form.cuit,'pharmacy') .then (function() {
-            regEntity(form,'razonsocial','pharmacy') .then (function() {
-                 res.status(201).send('Farmacia Registrada') ;    }),
-                (function(error){
+    confirmEntity(form.cuit,'pharmacy').then(
+        function(socialName) {
+            regEntity(form, socialName, 'pharmacy').then(
+                function() {
+                    res.status(201).send('Farmacia Registrada') ;
+                },
+                function(error){
                     console.log('regError');
                     res.status(400).send (error);
-                })
-
-
-    }),
-        (function(error){
-        res.status(400).send (error);
-})
-
-
+                });
+        },
+        function(error){
+            res.status(400).send (error);
+        });
 };
 router.post('/pharmacy', registerPharmacy);
 
@@ -56,9 +55,9 @@ var regEntity=function(form,socialName,entityName) {
     var def = q.defer();
 
     var arrayName=form.fullName.split(" ",2);
-    firstName=arrayName[0];
-    lastName=arrayName[1];
-    var bodyForm={street:form.street,number:form.number,city:form.city,name:socialName,province:form.province,fantasy_name:form.fantasyName,cuit:form.cuit,email:form.email,profile:{first_name:firstName,last_name:lastName}};
+    var firstName=arrayName[0];
+    var lastName=arrayName[1];
+    var bodyForm={street:form.street,number:form.number,city:form.city,name:socialName,province:form.province,phantasy_name:form.fantasyName,cuit:form.cuit,email:form.email,profile:{first_name:firstName,last_name:lastName}};
     request({
             method: 'post',
             url: config.registration_url+entityName,
@@ -69,17 +68,12 @@ var regEntity=function(form,socialName,entityName) {
         function(error, response, body){
 
 
-//    request.post(config.registration_url+entityName, form.street,form.number,form.city,socialName,form.province,form.fantasyName,form.cuit,form.email,arrayName, function (error, response, body) {
-
         var result = body;
         if (error)  {
             def.reject(error);
             console.log('error');
         }
-        else if (!result.valid){
-            def.reject('Invalid');
-        }
-
+       
         else {
 
             def.resolve();

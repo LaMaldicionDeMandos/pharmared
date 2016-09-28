e48d84f7da824662a031927bbc95493b010166ff
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-09-27 23:38:30 -0300

    Implementing bearer authentication strategy

--- server.js
+++ server.js
@@ -6,17 +6,34 @@ console.log('Scope: ' + process.env.NODE_ENV);
 config = xnconfig.parse(process.env.NODE_ENV, data);
 var bodyParser= require('body-parser');
 var express = require('express');
+var passport = require('passport');
+var BearerStrategy = require('passport-http-bearer').Strategy;
 var app = express();
+
 var register=require('./routers/register');
 
-
-
+passport.use(new BearerStrategy(
+    function(token, done) {
+      //TODO get user
+//      db.users.findByToken(token, function(err, user) {
+//        if (err) { return done(err); }
+//        if (!user) { return done(null, false); }
+        return done(null, {name: 'mockupUser'});
+//      });
+    }));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(require('morgan')('combined'));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/authenticate',
+    passport.authenticate('bearer', { session: false }),
+    function(req, res) {
+      res.send('User authenticated ' + JSON.stringify(req.user));
+    });
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');

cbddae7b6977ddd15d7832eb3eb2fda3968f4252
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-09-28 23:43:10 -0300

    Saving session and setting cookie

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ console.log('Scope: ' + process.env.NODE_ENV);
 config = xnconfig.parse(process.env.NODE_ENV, data);
 var bodyParser= require('body-parser');
 var express = require('express');
+var session = require('express-session');
 var passport = require('passport');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
 var HashStrategy = require('passport-hash').Strategy;
 var request = require('request');
 var app = express();
@@ -22,20 +25,42 @@ passport.use(new HashStrategy(
             if (res.statusCode != 200) {
                 done(new Error('bar request'));
             }
-            done(null, body);
+            var user = JSON.parse(body);
+            user.accessToken = hash;
+            done(null, user);
         });
     }));
+passport.serializeUser(function(user, done) {
+    console.log('Serializing user');
+    var json = JSON.stringify(user);
+    done(null, json);
+});
+
+passport.deserializeUser(function(json, done) {
+    var user = JSON.parse(json);
+    done(null, user);
+});
+app.enable('trust proxy');
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride());
+app.use(cookieParser('farmared_landing_secret'));
+app.use(session({
+    secret: 'farmared_landing_secret',
+    name: 'cookie_pharmared_landing',
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    cookie: {maxAge: config.session_expire, secure: false}}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/authenticate/:hash',
-    passport.authenticate('hash', { failureRedirect: '/', session: false }),
+    passport.authenticate('hash', { failureRedirect: '/', session: true }),
     function(req, res) {
-      res.redirect('http://www.google.com');
+      res.redirect(config.success_url);
     });
 
 app.get('/', ensureAuthenticated, function(req, res) {
@@ -50,7 +75,7 @@ app.get('/errors', function(req, res) {
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         console.log("User is authenticated");
-        return res.redirect('http://www.google.com');
+        return res.redirect(config.success_url);
         /*
         if(req.cookies.currentCountry && req.user.countries.indexOf(req.cookies.currentCountry)>=0) {
             req.user.currentCountry = req.cookies.currentCountry;
